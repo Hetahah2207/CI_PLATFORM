7cbf201cffa2df80e41a75ec57c378fdb3b9a67c
shah het <hetshah2207@gmail.com>
2023-03-31 10:04:02 +0530

    31-3

---
+++ CI-PLATFORM.Entities/ViewModels/MissionListingViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CI_PLATFORM.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_PLATFORM.Entities.ViewModels
+{
+    public class MissionListingViewModel
+    {
+        public Mission missions { get; set; }
+
+        public List<MissionMedium>? missionmedias { get; set; }
+
+        public List<MissionDocument>? missiondocuments { get; set; }
+
+        public List<Mission> relatedmissions { get; set; }
+        //public List<MissionRating>? missionratings { get; set; }
+   
+        public List<MissionSkill>? missionskills { get; set; }
+        public List<MissionApplication> missionapplications { get; set; }
+    
+        public List<Comment> comments { get; set; }
+        public List<FavoriteMission> favoriteMissions { get; set; }
+        public string commentDescription { get; set; }
+        public List<User>? coworkers { get; set; }
+    }
+}
