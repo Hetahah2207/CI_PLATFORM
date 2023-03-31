aa97a376c6a5725ffede996f3342407fea1c0736
shah het <hetshah2207@gmail.com>
2023-03-31 10:05:28 +0530

    31

--- CI_PLATFORM-main/CIPLATFORM/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CIPLATFORM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
